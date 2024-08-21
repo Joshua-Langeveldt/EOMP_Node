3fedf3ccc2d20405907ee28814aa902b653376ab
Joshua Langeveldt <langeveldt20joshua@gmail.com>
2024-08-21 12:05:17 +0200

    Initial Backend Commits

--- middleware/AuthenticateUser.js
+++ middleware/AuthenticateUser.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import jwt from 'jsonwebtoken'
-const { sign } = jwt
+const { sign, verify } = jwt
 
 function createToken(user) {
     return sign(
@@ -14,24 +14,24 @@ function createToken(user) {
         }
     )
 }
-// function verifyAToken(req, res, next) {
-//     const token = req?.headers["authorization"]
-//     if (token) {
-//         if (verify(token, process.env.SECRET_KEY)) {
-//             next()
-//         } else {
-//             res?.json({
-//                 status: res.statusCode,
-//                 msg: "Please provide the correct credentials."
-//             })
-//         }
-//     } else {
-//         res?.json({
-//             status: res.statusCode,
-//             msg: "Please login."
-//         })
-//     }
-// }
+function verifyAToken(req, res, next) {
+    const token = req?.headers["authorization"]
+    if (token) {
+        if (verify(token, process.env.SECRET_KEY)) {
+            next()
+        } else {
+            res?.json({
+                status: res.statusCode,
+                msg: "Please provide the correct credentials."
+            })
+        }
+    } else {
+        res?.json({
+            status: res.statusCode,
+            msg: "Please login."
+        })
+    }
+}
 
 export {
     createToken
